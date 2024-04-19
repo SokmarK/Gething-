31669fc7b18f7539eefe865e65263e9e80dcb5b3
SokmarK <hleontorres05@gmail.com>
2024-04-18 20:18:08 -0500

    Index terminado, siguiente: merge denuevo desde juegos a otros

--- Assets/JS/code.js
+++
@@ -1,7 +0,0 @@
-let inicio = document.querySelector("#contador").innerText
-inicio = parseInt(inicio)
-
-setInterval(() => {
-    inicio--
-    document.querySelector("#contador").innerText = inicio
-}, 1000);
